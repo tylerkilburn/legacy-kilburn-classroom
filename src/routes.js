7f8815d8dde224d7ca6f5b057f63f3b4b085d7cd
Tyler Kilburn <tylerkilburn@Tylers-MBP.home>
2016-10-08 21:35:06 -0400

    Refactored the routes page, put HBS helpers in there own module.

--- src/routes.js
+++ src/routes.js
@@ -1,102 +1,37 @@
+var _ = require('lodash');
 var hbs  = require('hbs');
+var studentData = require('../data/students.json');
+var hbsHelpers  = require('./hbsHelpers');
 
 module.exports = function() {
   const app = this;
 
-  // render home page
-  //app.get('/', (req, res) => res.render('index'));
-
-  function shuffle(array) {
-      let counter = array.length;
-      // While there are elements in the array
-      while (counter > 0) {
-          // Pick a random index
-          let index = Math.floor(Math.random() * counter);
-
-          // Decrease counter by 1
-          counter--;
-
-          // And swap the last element with it
-          let temp = array[counter];
-          array[counter] = array[index];
-          array[index] = temp;
-      }
-      return array;
-  }
-
-  //Body Parser
-  var bodyParser = require('body-parser');
-  app.use(bodyParser.json() );       // to support JSON-encoded bodies
-  app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-    extended: true
-  }));
-
-
-  //Get Student Data
-  var studentData = require('../data/students.json');
-
-  var period = '';
-  var groupSize = '';
-
-
   //HBS helpers
-  hbs.registerHelper('student-group', function(context, options) {
-    var ret = '';
-    var studentGroupSize = groupSize;
-    var isOpen = false;
-    for(var i=0, j=context.length; i<j; i++) {
-      if(i % studentGroupSize === 0) {
-        ret = ret + '<div class="col-sm-3"><div class="well">';
-        isOpen=true;
-      }
-      ret += options.fn(context[i]);
-      ret += '<br />';
-      if(i % studentGroupSize === studentGroupSize-1) { ret = ret + '</div></div>'; isOpen=false; }
-    }
-    if(isOpen) { ret = ret + '</div>'; }
-    return ret;
+  hbsHelpers.register();
+
+  //Home Page
+  app.get('/', function (req, res) {
+    res.redirect('group-work');
   });
 
-  //Ready Page
-  app.get('/', function (req, res) {
-    period = req.query.period;
-    groupSize = req.query.groupSize;
-
-    app.locals.period = period;
-    app.locals.groupSize = groupSize;
-
-    //Prep data
-    hbs.localsAsTemplateData(app);
+  //Group Page
+  app.get('/group-work', function (req, res) {
+    app.locals.period = req.query.period;
+    app.locals.groupSize = req.query.groupSize;
     app.locals.students =
-      shuffle(
+      _.shuffle(
         studentData.data
           .map(function(student) {
             student.lastName = student.lastName.substring(0,1) + '.';
-            if(student.nickname !== "")
+            if(student.nickname !== '') {
               student.firstName = student.nickname;
+            }
             return student;
           })
           .filter(function(student) {
-            return student.period == period;
+            return student.period === parseInt(req.query.period);
           })
       );
-
-    //Exclude partials
-    res.render('index', {layout: false});
+    res.render('group-work');
   });
-  //Ready Page
-  // app.get('/', function (req, res) {
-  //   //Exclude partials
-  //   res.render('index', {layout: false});
-  // });
-
-  // // render our list of messages retrieving them from our service
-  // app.get('/message-list', (req, res, next) => {
-  //   app.service('messages').find({}).then(result => {
-  //     // This handles paginated and non-paginated services
-  //     res.render('messages', { messages: result.data ? result.data : result });
-  //   }).catch(next);
-  // });
-
-
 };
