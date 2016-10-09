fcd9281162f22224ea56811a5d7ae9978a0707be
Tyler Kilburn <tylerkilburn@Tylers-MBP.home>
2016-10-09 02:03:59 -0400

    Updates to packages.json

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 
 const app = require('./app');
 const port = app.get('port');
-const server = app.listen(port);
+const server = app.listen(process.env.PORT);
 
 server.on('listening', () =>
   console.log(`Feathers application started on ${app.get('host')}:${port}`)
