99cd1f57fbcd44c99e6f6c8e7d8ff490fe503bb8
Tyler Kilburn <TylerKilburn@gmail.com>
2018-09-03 15:53:49 -0400

    Updated student roster.

--- src/index.js
+++ src/index.js
@@ -5,5 +5,5 @@ const port = app.get('port');
 const server = app.listen(process.env.PORT || 5000);
 
 server.on('listening', () =>
-  console.log(`Feathers application started on ${app.get('host')}:${port}`)
+  console.log(`Application started on http://${app.get('host')}:${port}`)
 );
